e1baf2e3d63ba51e5806a13e03ca779fde0986b5
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2019-09-29 21:29:29 +0300

    feat: add my solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  //Разобъем наши числа-строки на массивы и реверсируем их
+  let arrFirst = first.split("").reverse();
+  let arrSecond = second.split("").reverse();
+
+  //Заведём стек для перемноженных в столбик значений
+  let stackValues = [];
+
+  for(let i = 0; i < arrFirst.length; i++) {
+    for(let j = 0; j < arrSecond.length; j++) {
+      let value = arrFirst[i] * arrSecond[j];
+      //Если в стеке на данной позиции есть значение, то плюсуем его, если нет, то записываем
+      if(stackValues[i + j]) {
+        stackValues[i + j] = stackValues[i + j] + value;
+      } else {
+        stackValues[i + j] = value;
+      }
+    }
+  }
+  //Теперь имитируем стадию сложения при умножении в столбик, десятки, сотни и тысячи переносим выше на позицию
+  for(let i = 0; i < stackValues.length; i++) {
+    let a = stackValues[i] % 10;
+    let up = Math.floor(stackValues[i] / 10);
+    stackValues[i] = a;
+
+    if(stackValues[i + 1]) {
+      stackValues[i + 1] += up;
+    } else if(up != 0) {
+      stackValues[i + 1] = up;
+    }
+  }
+  //Получаем результат
+  let result = stackValues.reverse().join("");
+  
+  return result;
 }
